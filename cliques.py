4bc600fc6e2bc3e12169ab25e60551ff5022feef
hector1618 <hector1618@gmail.com>
2011-03-13 16:11:01 +0530

    Added cliques.py - a file containing all the functions

---
+++ cliques.py
@@ -0,0 +1,9 @@
+#! usr/bin/python
+
+#This file will be used as main file which contains all the functions
+
+import all_maximal_cliques
+#import coloring.py
+#import is_clique_k.py
+#import max_clique.py
+#import random_graph.py
