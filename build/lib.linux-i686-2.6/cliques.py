db36be951ea3f2ea719d40176a4a95e92af9609d
hector1618 <hector1618@gmail.com>
2011-03-13 16:42:36 +0530

    Little modification in cliques.py and README

--- build/lib.linux-i686-2.6/cliques.py
+++ build/lib.linux-i686-2.6/cliques.py
@@ -2,7 +2,7 @@
 
 #This file will be used as main file which contains all the functions
 
-from all_maximal_cliques import all_maximal_cliques
+import all_maximal_cliques
 #import coloring.py
 #import is_clique_k.py
 #import max_clique.py
