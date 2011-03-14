cf5c45320c3d11101e13e87cfedd5d8f24f732d9
hector1618 <hector1618@gmail.com>
2011-03-14 22:49:40 +0530

    Fixed bug in cliques.py

--- cliques.py
+++ cliques.py
@@ -6,4 +6,4 @@ from all_maximal_cliques import all_maximal_cliques
 #import coloring.py
 #import is_clique_k.py
 #import max_clique.py
-from random_graphs import random_graph.py
+from random_graphs import random_graph
