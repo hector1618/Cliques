0328ddcd1b01bed664070276ef5de4abbef5cea8
hector1618 <hector1618@gmail.com>
2011-03-14 23:00:29 +0530

    Added a function is_clique_k to cliques.py

--- cliques.py
+++ cliques.py
@@ -4,6 +4,6 @@
 
 from all_maximal_cliques import all_maximal_cliques
 #import coloring.py
-#import is_clique_k.py
+from is_clique_k import is_clique_k
 #import max_clique.py
 from random_graphs import random_graph
